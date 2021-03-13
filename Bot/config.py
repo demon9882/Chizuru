9688c04613ffeb450cefda87758fa4ab74bbab5a
Demon <74318897+demon9882@users.noreply.github.com>
2021-03-13 12:30:59 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -1,8 +1,8 @@
 class Config(object):
     LOGGER = True
-    TOKEN = ""
-    DB_URI = ""
-    OWNER_ID = 895373440
+    TOKEN = "1658765294:AAFaCpdkFxzCmDwBgbJmsDSDxRcigTBN1mA"
+    DB_URI = "postgres://pbhnwssiubuske:064155610519ea8957d1ad156cdb12d83206c0315e0500c7f3a61ca33344d9d6@ec2-52-6-178-202.compute-1.amazonaws.com:5432/d860sv8343r6iv"
+    OWNER_ID = 680241924
 
 
 class Production(Config):
